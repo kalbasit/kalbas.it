f8a65c05d8000fc2d85189256b1bbda525a81f18
Wael M. Nasreddine <wael@getpublica.com>
2017-01-18 16:33:54 -0800

    replace the install with a script

---
+++ scripts/install_hugo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+set -e
+
+mkdir -p "${GOPATH}/src/github.com/spf13"
+git clone --branch=$HUGO_VERSION https://github.com/spf13/hugo.git "${GOPATH}/src/github.com/spf13/hugo"
+cd "${GOPATH}/src/github.com/spf13/hugo"
+go get github.com/kardianos/govendor
+govendor sync github.com/spf13/hugo
+make install
