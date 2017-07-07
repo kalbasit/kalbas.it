77ab6b68009b48205830fdd88ab144b7951f9a0c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-07-07 14:45:59 -0700

    travis: bump hugo to v0.25

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -2,9 +2,7 @@
 set -ex
 
 mkdir -p "${GOPATH}/src/github.com/spf13"
-git clone --branch=$HUGO_VERSION https://github.com/spf13/hugo.git "${GOPATH}/src/github.com/spf13/hugo"
+git clone --branch="${HUGO_VERSION}" https://github.com/spf13/hugo.git "${GOPATH}/src/github.com/spf13/hugo"
 cd "${GOPATH}/src/github.com/spf13/hugo"
-go get github.com/kardianos/govendor
-govendor sync github.com/spf13/hugo
-make install
+make vendor install
 pip install -r requirements.txt
