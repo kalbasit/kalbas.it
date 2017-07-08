f99491a2ecfcbc8875b703d3cf887af36f1a9951
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-07-07 19:51:10 -0700

    the repo has moved

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir -p "${GOPATH}/src/github.com/spf13"
-git clone --branch="${HUGO_VERSION}" https://github.com/spf13/hugo.git "${GOPATH}/src/github.com/spf13/hugo"
-cd "${GOPATH}/src/github.com/spf13/hugo"
+mkdir -p "${GOPATH}/src/github.com/gohugoio"
+git clone --branch="${HUGO_VERSION}" https://github.com/gohugoio/hugo.git "${GOPATH}/src/github.com/gohugoio/hugo"
+cd "${GOPATH}/src/github.com/gohugoio/hugo"
 make vendor install
 pip install -r requirements.txt
