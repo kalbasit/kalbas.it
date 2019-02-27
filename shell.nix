58ba8b46f270d0a555417e6176108d035beb2d04
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-27 08:26:35 -0800

    add a new theme, gruvbox

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ in
 with pkgs;
 
 let
+  hugo-theme-gruvbox = pkgs.callPackage ./pkgs/themes/gruvbox {};
   hugo-theme-terminal = pkgs.callPackage ./pkgs/themes/terminal {};
 
   hugoConfig = {
@@ -24,6 +25,43 @@ let
     paginate = "10";
     title = "kalbasit";
     permalinks.post = "/:year/:month/:day/:slug";
+  } // lib.optionalAttrs (theme == "gruvbox") {
+    params = {
+      author = "Wael Nasreddine";
+      description = "Father - Entrepreneur - Architect - Engineer";
+      keywords = [
+        "blog"
+        "golang"
+        "gopher"
+        "kalbasit"
+        "programmer"
+        "software architect"
+        "software engineering"
+      ];
+      titlePrefix = "";
+      titleSuffix = " | kalbas.it";
+      dateFormat = "2006-01-02";
+      menuItemPrefix = "~/";
+      postTruncateLength = 250;
+      hideGopher = false;
+      disqusAutoLoad = true;
+      disqusAutoLoadCount = true;
+      googleAnalyticsTrackingId = "UA-82839578-2";
+
+      menu = {
+        links = {
+          name = "CV";
+          identifier = "cv.pdf";
+          url = "https://rawgit.com/bbrks/cv/master/cv.pdf";
+          weight = 9999;
+        };
+        social = {
+          name = "LinkedIn";
+          url = "https://uk.linkedin.com/in/bbrks";
+          weight = 100;
+        };
+      };
+    };
   } // lib.optionalAttrs (theme == "terminal") {
     params = {
       contentTypeName = "post";
